6954171936041f0b56a4835a674e6ecdb2cbb339
Abhishek Kuntal <128124180+abhishiek2002@users.noreply.github.com>
2024-10-13 14:44:14 +0530

    Project 9

---
+++ 9. Auto Text Effect Animation/app.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container');
+
+const careers = ["youtuber", "web developer", "freelancer", "instructor"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText(){
+
+    characterIndex++;
+
+    container.innerHTML = `
+    <h1>I am ${careers[careerIndex]==="instructor"?"an":"a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>
+    `;
+
+    if (characterIndex === careers[careerIndex].length){
+        careerIndex++;
+        characterIndex = 0;
+    };
+
+    if(careerIndex === careers.length) careerIndex = 0;
+    
+    setTimeout(updateText,400);
+
+}
+
+
