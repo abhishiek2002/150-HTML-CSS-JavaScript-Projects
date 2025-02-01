b75d3bdbc60e011b606224c16ee78d5d454dfcb7
Abhishek Kuntal <128124180+abhishiek2002@users.noreply.github.com>
2025-02-01 15:59:10 +0530

    18

---
+++ 18. Random Color Generator/app.js
@@ -0,0 +1,42 @@
+const containerEl = document.querySelector('.container');
+
+
+for (let index = 0; index < 30; index++) {
+    const colorContainerEl = document.createElement("div");
+
+    colorContainerEl.classList.add('color-container');
+
+    containerEl.appendChild(colorContainerEl);
+};
+
+
+const colorContainerEls = document.querySelectorAll('.color-container');
+
+generateColors();
+
+function generateColors() {
+    colorContainerEls.forEach((colorContainerEl) =>{
+        
+        const newCode = randomColor();
+        colorContainerEl.style.backgroundColor = '#' + newCode;
+        colorContainerEl.innerText = '#' + newCode;
+    })
+}
+
+function randomColor() {
+    const chars = "0123456789abcdef";
+
+    const colorCodeLength = 6;
+
+    let colorCode = "";
+
+    for (let index = 0; index < colorCodeLength; index++) {
+
+        const randomNum = Math.floor(Math.random() * chars.length);   
+
+        colorCode += chars.substring(randomNum,randomNum+1);
+        // console.log(colorCode, randomNum);
+    };
+
+    return colorCode;
+}
